db9d4ba545415bd2a9dce14220c68644c61c15b2
Chandrakant Karale <chandrakantk@gmail.com>
2018-07-02 20:42:09 +0530

    Adding the model classes.

---
+++ src/com/ccompany/model/Department.java
@@ -0,0 +1,35 @@
+
+package com.ccompany.model;
+
+
+public class Department {
+
+		private String name;
+		private String address;
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return name;
+		}
+		/**
+		 * @param name the name to set
+		 */
+		public void setName(String name) {
+			this.name = name;
+		}
+		/**
+		 * @return the address
+		 */
+		public String getAddress() {
+			return address;
+		}
+		/**
+		 * @param address the address to set
+		 */
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		
+}
---
+++ src/com/ccompany/model/Student.java
@@ -0,0 +1,32 @@
+
+package com.ccompany.model;
+
+public class Student {
+	private String name;
+	private String address;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
